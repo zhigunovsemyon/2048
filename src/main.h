6351e57c4fcc907e0e1c782a19008454c7a007a5
zhigunovsemyon <zhigunovsemen@gmail.com>
2024-04-01 20:02:27 +0300

    Изменено название проекта, обновлён main.h

--- src/main.h
+++ src/main.h
@@ -5,3 +5,22 @@
 #include <SDL2/SDL.h>
 
 enum Errors {NOERR, SDLERR};
+
+typedef struct
+{
+	Uint64* Field;
+	Uint64 Score;
+}
+Game;
+
+typedef struct 
+{
+	Uint8 quit;
+	Uint8 left;
+	Uint8 righ;
+	Uint8 up;
+	Uint8 down;
+	Uint8 mouse;
+}
+Controls;
+
