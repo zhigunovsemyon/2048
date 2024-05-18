fcf8e36868a641ccbe27392c2d258ac7bcfa52a3
zhigunovsemyon <zhigunovsemen@gmail.com>
2024-05-18 12:52:43 +0300

    Иправил косяк с сравнением -1 элемента в checkdowncombo. Под Windows работает

--- src/misc.c
+++ src/misc.c
@@ -77,7 +77,7 @@ Uint8 CheckDownCombo(Game *Game, Params *Params)
 	// Цикл перебора каждого столбца
 	for (Sint8 j = 0; j < Game->FieldSize; j++)
 	{ // Цикл перебора каждой строки
-		for (Sint8 i = Game->FieldSize - 1; i >= 0; i--)
+		for (Sint8 i = Game->FieldSize - 1; i > 0; i--)
 		{ // Если данная ячейка пустая
 			if (!Game->Field[i * Game->FieldSize + j].val /* == 0 */)
 				continue;
